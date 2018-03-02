8218e4cee5829d2a2a3efd9e5971837f0ca70a81
Baspar <bastien.laine@insa-rouen.fr>
2018-03-02 18:42:04 +0800

    [add] Dummy data

--- src/resa/db.cljs
+++ src/resa/db.cljs
@@ -1,1 +1,18 @@
 (ns resa.db)
+
+(def booked-slot
+  (let [today (.. js/window moment)
+        day-in (fn [d]
+                 (.. today
+                     (add d "day")
+                     (format "YYYY-MM-DD")))]
+    ;; Format YYYY-MM-DD
+    {(day-in 0) {12 [0 15 30 45]
+                 13 [0 15 45]
+                 20 [0 30 45]}
+     (day-in 2) {20 [0 30]
+                 13 [15 30]}
+     (day-in 3) {20 [0 30]
+                 13 [15 30]}
+     (day-in 5) {20 [0 30]
+                 13 [15 30]}}))
