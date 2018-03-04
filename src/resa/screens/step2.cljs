6aa2b2a2f60970c92bb35609fba1c00fabd6e489
Baspar <bastien.laine@insa-rouen.fr>
2018-03-05 01:14:31 +0800

    [add] Default screen 2

--- src/resa/screens/step2.cljs
+++ src/resa/screens/step2.cljs
@@ -6,35 +6,44 @@
 
 (defc screen2
   [store]
-  (let [{:keys [pax time name phone email]} (:data @store)]
-    [:div
-     [:br]
-     (ant/input
-       {:placeholder "Your name"
-        :type "text"
-        :auto-focus true
-        :value (or name "")
-        :style {:margin 5}
-        :on-change #(swap! store assoc-in [:data :name]
-                           (-> % .-target .-value))})
-     [:br]
-     (ant/input
-       {:placeholder "Your phone number"
-        :type "tel"
-        :value (or phone "")
-        :style {:margin 5}
-        :on-change #(swap! store assoc-in [:data :phone]
-                           (-> % .-target .-value))})
-     [:br]
-     (ant/input
-       {:placeholder "Your e-mail"
-        :type "email"
-        :value (or email "")
-        :style {:margin 5}
-        :on-change #(swap! store assoc-in [:data :email]
-                           (-> % .-target .-value))})
-
-     [:br]
-     (ant/button {:style {:width "100%" :margin 5} :on-click #(dispatch! store :h-back-to-step1)} "< Back")
-     (ant/button {:style {:width "100%" :margin 5} :on-click #(dispatch! store :h-book)} "Book!")
-     [:br]]))
+  (let [m @store
+        data (get m :data {})
+        {:keys [pax time name phone email]} data]
+    [:div {:style {:display "flex" :flex-direction "column"}}
+     ;; Title
+     [:div {:style {:display "flex" :align-items "center"}}
+      (ant/select {:default-value "Mr." :on-change #(swap! store assoc-in [:data :title] %)}
+                  (ant/select-option {:value "Mr."} "Mr.")
+                  (ant/select-option {:value "Mrs."} "Mrs.")
+                  (ant/select-option {:value "Ms."} "Ms."))
+      (ant/input {:placeholder "Your name"
+                  :type "text"
+                  :auto-focus true
+                  :value (or name "")
+                  :on-change #(swap! store assoc-in [:data :name]
+                                     (.. % -target -value))})]
+     ;; Phone number
+     (ant/input {:placeholder "Your phone number"
+                 :type "tel"
+                 :value (or phone "")
+                 :on-change #(swap! store assoc-in [:data :phone]
+                                    (.. % -target -value))})
+     ;; Email
+     (ant/input {:placeholder "Your e-mail"
+                 :type "email"
+                 :value (or email "")
+                 :on-change #(swap! store assoc-in [:data :email]
+                                    (.. % -target -value))})
+     ;; Number pax
+     (ant/input {:type "number"
+                 :placeholder "Number of guests"
+                 :min 1
+                 :value (or pax "")
+                 :on-change #(swap! store assoc-in [:data :pax]
+                                    (.. % -target -value))})
+     (ant/button {:style {:width "100%"}
+                  :on-click #(dispatch! store :h-back-to-step1)}
+                 "< Back")
+     (ant/button {:style {:width "100%"}
+                  :on-click #(dispatch! store :h-book)}
+                 "Book!")]))
