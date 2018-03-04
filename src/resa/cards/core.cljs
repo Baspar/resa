fb9dcac6a3efdc6a3a93c2373553b852f4d07184
Baspar <bastien.laine@insa-rouen.fr>
2018-03-04 16:08:14 +0800

    [edit] Change devcards to be per-screen

--- src/resa/cards/core.cljs
+++ src/resa/cards/core.cljs
@@ -1,29 +1,32 @@
 (ns resa.cards.core
   (:require
     [resa.utils :refer [wrap-en]]
-    [resa.screens.core :refer [ui-screen]])
+    [resa.screens.step1 :refer [screen1]]
+    [resa.screens.step2 :refer [screen2]]
+    [resa.screens.step3 :refer [screen3]]
+    [resa.screens.step4 :refer [screen4]])
   (:require-macros
     [devcards.core :as dc :refer [defcard deftest]]))
 
 ;;; cards
 (defcard step1
   (fn [store _]
-    (wrap-en (ui-screen store)))
+    (wrap-en (screen1 store)))
   {:screen :step1}
   {:inspect-data true})
 (defcard step2
   (fn [store _]
-    (wrap-en (ui-screen store)))
+    (wrap-en (screen2 store)))
   {:screen :step2}
   {:inspect-data true})
 (defcard step3
   (fn [store _]
-    (wrap-en (ui-screen store)))
+    (wrap-en (screen3 store)))
   {:screen :step3}
   {:inspect-data true})
 (defcard step4
   (fn [store _]
-    (wrap-en (ui-screen store)))
+    (wrap-en (screen4 store)))
   {:screen :step4}
   {:inspect-data true})
 
