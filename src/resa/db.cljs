0d17961f02fc3b0fe57bf6a4a03a2f81dfd2f311
Unknown <manzano.maxime@gmail.com>
2018-03-05 16:11:31 +0800

    [add] slot generator

--- src/resa/db.cljs
+++ src/resa/db.cljs
@@ -1,5 +1,33 @@
 (ns resa.db)
 
+(defn day-gen
+  [d]
+  (let [today (.. js/window moment) ]
+    (.. today
+      (add d "day")
+      (format "YYYY-MM-DD"))))
+
+(defn x
+  [a]
+
+  (->> (range 60)
+       (take-nth 15)
+       (filterv  #(> (rand) prob))
+       (assoc {} a )))
+
+(defn slot-gen
+  []
+  (let [morning (->> (range 11 15)
+                     (map x )
+                     (into {}))
+        evening (->> (range 19 23)
+                     (map x )
+                     (into {}))
+        days (->> (range 200)
+                  (map (fn[x] {(day-gen x) (merge morning evening)}))
+                  (apply merge))]
+        days))
+
 (def avaliable-slot
   (let [today (.. js/window moment)
         day-in (fn [d]
