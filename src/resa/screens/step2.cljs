bb34773197625d688bb1d68b5ddc8278a0bdecd2
Baspar <bastien.laine@insa-rouen.fr>
2018-03-05 17:28:52 +0800

    [edit] Change step2 to reaction

--- src/resa/screens/step2.cljs
+++ src/resa/screens/step2.cljs
@@ -16,7 +16,7 @@
      ;; Title
      [:br]
      [:div {:style {:display "flex" :align-items "center"}}
-      (ant/select {:default-value "Mr." :on-change #(swap! store assoc-in [:data :title] %)}
+      (ant/select {:default-value "Mr." :on-change #(dispatch! store [:step2--set-title %])}
                   (ant/select-option {:value "Mr."} "Mr.")
                   (ant/select-option {:value "Mrs."} "Mrs.")
                   (ant/select-option {:value "Ms."} "Ms."))
@@ -24,27 +24,23 @@
                   :type "text"
                   :auto-focus true
                   :value (or name "")
-                  :on-change #(swap! store assoc-in [:data :name]
-                                     (.. % -target -value))})]
+                  :on-change #(dispatch! store [:step2--set-name %])})]
      ;; Phone number
      (ant/input {:placeholder "Your phone number"
                  :type "tel"
                  :value (or phone "")
-                 :on-change #(swap! store assoc-in [:data :phone]
-                                    (.. % -target -value))})
+                 :on-change #(dispatch! store [:step2--set-phone %])})
      ;; Email
      (ant/input {:placeholder "Your e-mail"
                  :type "email"
                  :value (or email "")
-                 :on-change #(swap! store assoc-in [:data :email]
-                                    (.. % -target -value))})
+                 :on-change #(dispatch! store [:step2--set-email %])})
      ;; Number pax
      (ant/input {:type "number"
                  :placeholder "Number of guests"
                  :min 1
                  :value (or pax "")
-                 :on-change #(swap! store assoc-in [:data :pax]
-                                    (.. % -target -value))})
+                 :on-change #(dispatch! store [:step2--set-pax %])})
      [:br]
      (ant/button {:style {:height "4em"}
                   :on-click #(dispatch! store :go-forward)
