3beaf400d7167783dbc9ebb7a5e7eaf57a74be72
Baspar <bastien.laine@insa-rouen.fr>
2018-03-07 12:25:03 +0800

    [edit] Change script

--- gh-page-deploy.sh
+++ gh-page-deploy.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
+PROJ_PATH="/tmp/tmp_build"
+PROJ_BUILD="resources/public/*"
 
-echo "Please chose en environement to build [dev/prod] (default: prod)"
+
+if curl -s localhost:3450 >> /dev/null
+then
+    echo "Please stop Lein before pushing"
+    exit 1
+fi
+
+echo "Please chose en environment to build [dev/prod] (default: prod)"
 read envBuild
 if [[ $envBuild != "build" || $envBuild != "prod" ]]
 then
     envBuild="prod"
 fi
+
 lein clean && lein cljsbuild once $envBuild
-path="/tmp/tmp_build"
-build="resources/public/*"
-rm -rf $path
-mkdir -p $path
-cp -r $build $path
+rm -rf $PROJ_PATH
+mkdir -p $PROJ_PATH
+cp -r $PROJ_BUILD $PROJ_PATH
 git checkout gh-pages
+
 echo "Preparing repo..."
-rm -rf images/ css/ js/ index.html card.html
-mv $path/* .
-# mv resa/images/ .
-rm -rf js/compiled/out
+git rm -rf *
+rm -rf *
+mv $PROJ_PATH/* .
+
+# rm -rf js/compiled/out
 echo "Do you wanna do an automatic commit and push to gh-pages branches ? [y/n] (default: no)"
 read answer
-if [[ $answer == "yes" ]]
-then
-    git add --all
-    git commit -am "automatic deployement on gh-pages"
-    git push origin gh-pages
-    git checkout master
-fi
-
+# if [[ $answer == "yes" ]]
+# then
+#     git add --all
+#     git commit -am "automatic deployement on gh-pages"
+#     git push origin gh-pages
+#     git checkout master
+# fi
+#
 echo "Done."
